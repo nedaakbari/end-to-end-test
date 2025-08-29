1ed4b546c307c6be3e0314fc575f768b8e0e1f1d
nedaakbari <nedaakbari555@gmail.com>
2025-08-29 15:41:46 +0330

    update

--- sample3/src/test/java/ValidatedResponseDemo.java
+++ sample3/src/test/java/ValidatedResponseDemo.java
@@ -18,7 +18,7 @@ public class ValidatedResponseDemo {
                 .then()
                 .statusCode(200)//inseted of assert.equal
                 .contentType(ContentType.JSON)
-                .header("x-ratelimit-limit","60");
+                .header("x-ratelimit-limit", "60");
     }
 
     @Test
@@ -74,11 +74,11 @@ public class ValidatedResponseDemo {
                 .then()
 //                .statusCode(200)
                 .statusCode(lessThan(300))
-                .statusCode(anyOf(equalTo(200),equalTo(202)))
-                .header("x-ratelimit-limit",notNullValue())
-                .header("x-ratelimit-limit",Matchers.not(emptyString()))
-                .header("x-ratelimit-limit",notNullValue())
-                .header("x-ratelimit-limit",containsStringIgnoringCase("x"));
+                .statusCode(anyOf(equalTo(200), equalTo(202)))
+                .header("x-ratelimit-limit", notNullValue())
+                .header("x-ratelimit-limit", Matchers.not(emptyString()))
+                .header("x-ratelimit-limit", notNullValue())
+                .header("x-ratelimit-limit", containsStringIgnoringCase("x"));
     }
 
     @Test
@@ -87,11 +87,27 @@ public class ValidatedResponseDemo {
                 .then()
                 .statusCode(200)
                 .statusCode(lessThan(300))
-                .header("cache-control",containsStringIgnoringCase("max-age=60"))
+                .header("cache-control", containsStringIgnoringCase("max-age=60"))
                 .time(lessThan(3L), TimeUnit.SECONDS)
-                .header("etag",notNullValue())
-                .header("etags",not(isEmptyString()))
-                .header("etag",not(emptyString()));
+                .header("etag", notNullValue())
+                .header("etags", is(emptyString()))
+                .header("etag", not(emptyString()));
+
+    }
+
+    Map<String, String> expectedHeaders = Map.of(
+            "content-encoding", "gzip",
+            "access-control-allow-origin", "*");
+
+    @Test
+    void response5() {
+        RestAssured.get(BASE_URL)
+                .then()
+
+                .headers("cache-control", containsStringIgnoringCase("public"),
+                        "content-encoding", "gzip",
+                        "access-control-allow-origin", "*")
+                .headers(expectedHeaders);
 
     }
 }
